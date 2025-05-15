f8bc87ea2db35a68ca2fee8fc96afccd0f284fde
faisalProject <mf494284@gmail.com>
2025-05-15 23:57:17 +0700

    fixing the classification disease page

--- front-end/src/scripts/pages/disease-classification/disease-classification-page.js
+++ front-end/src/scripts/pages/disease-classification/disease-classification-page.js
@@ -1,4 +1,6 @@
 export default class DiseaseClassificationPage {
+  #takenDocumentation = null;
+
   async render() {
     return `
       <div class="main-feature-content">
@@ -16,7 +18,7 @@ export default class DiseaseClassificationPage {
               <i class="bi bi-search"></i>
               <p>Rekomendasi</p>
             </div>
-            <div class="bottom-content">
+            <div class="bottom-content main-recommendation-content">
               <div class="item">
                 <p class="title">🌱 Daftar Penyakit yang Bisa Dideteksi</p>
                 <p>Tampilkan jenis-jenis penyakit tanaman umum yang bisa dikenali oleh sistem.</p>
@@ -34,6 +36,17 @@ export default class DiseaseClassificationPage {
                   <li>Usahakan untuk tidak ada objek lain yang menghalangi bagian tanaman yang ingin dideteksi.</li>
                 </ul>
               </div>
+              <div class="item">
+                <form id="form-upload">
+                  <div class="upload-button-container">
+                    <button class="btn camera-button">Ambil Gambar</button>
+                    <label for="image-file" class="btn file-button">Unggah Gambar</label>
+                    <input type="file" id="image-file" name="image-file" style="display: none">
+                  </div>
+                  <div class="image-preview" id="image-preview"></div>
+                  <div class="detection-button-container" id="detection-button-container"></div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
@@ -41,5 +54,70 @@ export default class DiseaseClassificationPage {
     `;
   }
 
-  async afterRender() {}
+  async afterRender() {
+    this.#setupForm();
+  }
+
+  #setupForm() {
+    const imageFile = document.querySelector('#image-file');
+    const imagePreview = document.querySelector('#image-preview');
+    const detectionButtonContainer = document.querySelector('#detection-button-container');
+
+    imageFile.addEventListener('change', () => {
+      const file = imageFile.files[0];
+
+      if (!file) return;
+
+      this.#takenDocumentation = {
+        blob: file,
+        url: URL.createObjectURL(file),
+      };
+
+      const img = document.createElement('img');
+      img.src = this.#takenDocumentation.url;
+      img.alt = 'preview';
+      imagePreview.innerHTML = '';
+      imagePreview.appendChild(img);
+
+      const detectionButton = document.createElement('button');
+      detectionButton.classList.add('detection-button');
+      detectionButton.type = 'button';
+      detectionButton.textContent = 'Deteksi Sekarang';
+      detectionButtonContainer.innerHTML = '';
+      detectionButtonContainer.appendChild(detectionButton);
+      
+      this.#setupDecationButton();
+    })
+
+  }
+
+  #setupDecationButton() {
+    const detectionButton = document.querySelector('.detection-button');
+    detectionButton ? detectionButton.addEventListener('click', (event) => {
+      event.stopPropagation();
+      const mainRecommendationContent = document.querySelector('.main-recommendation-content');
+      const item = document.createElement('div');
+      item.classList.add('item');
+      const title = document.createElement('p');
+      title.classList.add('title');
+      title.innerHTML = '🔍 Hasil';
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies posuere ipsum eget fermentum. Maecenas sodales urna eu accumsan sodales. Duis non ipsum mollis, feugiat sem tempor, dapibus magna.';
+      item.appendChild(title);
+      item.appendChild(paragraph);
+      mainRecommendationContent.appendChild(item);  
+
+
+      const item2 = document.createElement('div');
+      item2.classList.add('item');
+      const title2 = document.createElement('p');
+      title2.classList.add('title');
+      title2.innerHTML = '💡 Saran';
+      const paragraph2 = document.createElement('p');
+      paragraph2.innerHTML = 'Praesent nec consectetur neque, vel efficitur neque. Vestibulum non turpis a nisl ornare imperdiet vitae et nisi. Duis tincidunt lobortis tellus ac viverra. Sed suscipit varius imperdiet. Ut condimentum pretium odio, non blandit metus viverra id. Proin sollicitudin metus nec volutpat commodo.';
+      item2.appendChild(title2);
+      item2.appendChild(paragraph2);
+      mainRecommendationContent.appendChild(item2);
+    }) : null;
+  }
 }
