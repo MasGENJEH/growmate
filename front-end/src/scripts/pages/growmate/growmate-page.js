5a1a1517428d0249c660c5b87e116178e2f69c42
faisalProject <mf494284@gmail.com>
2025-05-25 00:36:36 +0700

    added about page

--- front-end/src/scripts/pages/growmate/growmate-page.js
+++ front-end/src/scripts/pages/growmate/growmate-page.js
@@ -2,7 +2,20 @@ export default class GrowmatePage {
   async render() {
     return `<div class="growmate-content">
       <div class="container">
-        <p>Welcome to the Growmate</p>
+        <div class="jumbotron">
+          <div class="description">
+            <h1>GrowMate</h1>
+            <p>GrowMate adalah aplikasi pintar yang membantu petani dan pecinta tanaman dalam mengidentifikasi hama, penyakit, serta memberikan rekomendasi perawatan secara akurat dan cepat.</p>
+          </div>
+          <div class="image-container">
+            <img src="images/person.png" alt="">
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+          </div>
+        </div>
+
+        <div class="main-content"></div>
       </div>
     </div>`
   }
